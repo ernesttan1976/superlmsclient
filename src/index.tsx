d24bc8f6968d4c5a91906f080b0dd0fd5cd5446b
ernesttan1976 <ernesttan1976@gmail.com>
2023-04-17 09:50:18 +0800

    removed node version 14

--- src/index.tsx
+++ src/index.tsx
@@ -7,10 +7,24 @@ import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container);
+
+let redirect_uri2 = "";
+
+redirect_uri2=window.location.origin;
+
+// if (window.location.origin==="http://localhost:3000"){
+//   redirect_uri2 = 'https://103.252.202.42:3000';
+// } else {
+//   redirect_uri2 = window.location.origin;
+// }
+console.log(window.location.origin, redirect_uri2 );
+
 root.render(
   <React.StrictMode>
-    <Auth0Provider domain="dev-ndcpsqh6dgrlwy4z.us.auth0.com" clientId="UgyE6aFSr8Teo3tuJR0u8XmXjtUJ8npr" authorizationParams={{redirect_uri: window.location.origin}}>
-      {window.location.origin}
+    <Auth0Provider 
+    domain="dev-ndcpsqh6dgrlwy4z.us.auth0.com" 
+    clientId="UgyE6aFSr8Teo3tuJR0u8XmXjtUJ8npr" 
+    authorizationParams={{redirect_uri: redirect_uri2}}>
     <App />
     </Auth0Provider>
   </React.StrictMode>
